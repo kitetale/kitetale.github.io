ed43c4fe771c1a247e78918aee4b9b9c3a3e8e99
Ashley Kim <kitetale248@gmail.com>
2022-07-07 23:30:44 -0700

    updated app header

--- src/component/AppHeader/AppHeader.js
+++ src/component/AppHeader/AppHeader.js
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from "react";
-import {appHeader,navLink, navLinkItem,} from "./AppHeader.module.css";
-import { Link } from 'react-router-dom';
+import {
+  appHeader,
+  header,
+  navLink,
+  navLinkItem,
+  logo,
+} from "./AppHeader.module.css";
+import { Link } from "react-router-dom";
+import AshleyKim from "../../images/AshleyKim.svg";
 
 const AppHeader = () => {
   return (
     <div className={appHeader}>
       <title>Ashley Kim</title>
-      <div>
-        <header>
-          <div>
-            <Link to="/">
-              <img src=""/>
-            </Link>
-          </div>
-        </header>
-        <nav>
-          <ul className={navLink}>
-            <li className={navLinkItem}>
-              <Link to="/projects">works</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/about">about</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/resume">resume</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <header className={header}>
+        <Link to="/">
+          <img src={AshleyKim} alt="Ashley Kim" className={logo} />
+        </Link>
+      </header>
+      <nav className={navLink}>
+        <ul className={navLink}>
+          <li className={navLinkItem}>
+            <Link to="/projects">works</Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about">about</Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/resume">resume</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import {app} from "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppHeader from "../component/AppHeader/AppHeader";
 import LandingPage from "./LandingPage";
