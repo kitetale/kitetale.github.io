df2a7186653845631498cefc01b26c8c321ef9aa
Ashley Kim <kitetale248@gmail.com>
2022-07-12 23:18:13 -0700

    updated resume page

--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { content, title } from "./ResumePage.module.css";
+import { content, title, resumeStyle, center } from "./ResumePage.module.css";
+import resume from "../../images/AshleyKimResume.png";
 
 const ResumePage = () => {
   return (
     <div className={content}>
       <div className={title}>RESUME</div>
+      <img src={resume} alt="Ashley Kim resume 2022" className={resumeStyle} />
+      <div>
+        <p className={center}>Download PDF</p>
+      </div>
     </div>
   );
 };
