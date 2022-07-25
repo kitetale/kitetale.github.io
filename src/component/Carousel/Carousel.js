d3ecf4a7225a6af60411262aa2f5ba42dbb3a62e
Ashley Kim <kitetale248@gmail.com>
2022-07-24 18:45:16 -0700

    [feat] working carousel

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
-import "./carousel.module.css";
+import {
+  container,
+  wrapper,
+  left,
+  right,
+  contentWrapper,
+  caroContent,
+} from "./Carousel.module.css";
 
 const Carousel = (props) => {
   const { children } = props;
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [length, setLength] = useState(children.length);
 
@@ -16,6 +22,7 @@ const Carousel = (props) => {
       setCurrentIndex((prevState) => prevState + 1);
     }
   };
+
   const prev = () => {
     if (currentIndex > 0) {
       setCurrentIndex((prevState) => prevState - 1);
@@ -23,23 +30,27 @@ const Carousel = (props) => {
   };
 
   return (
-    <div className="carousel-container">
-      <div className="carousel-wrapper">
+    <div className={container}>
+      <div className={wrapper}>
         {currentIndex > 0 && (
-          <button className="left-arrow" onClick={prev}>
+          <button className={left} onClick={prev}>
             &lt;
           </button>
         )}
-        <div className="carousel-content-wrapper">
+
+        <div className={contentWrapper}>
           <div
-            className="carousel-content"
-            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+            className={caroContent}
+            style={{
+              transform: `translateX(-${currentIndex * 100}%)`,
+              transition: "1.5s",
+            }}
           >
             {children}
           </div>
         </div>
         {currentIndex < length - 1 && (
-          <button className="right-arrow" onClick={next}>
+          <button className={right} onClick={next}>
             &gt;
           </button>
         )}
--- src/pages/works.js
+++ src/pages/works.js
@@ -1,29 +1,25 @@
 import React from "react";
-import {
-  content,
-  top,
-  title,
-  tabs,
-  images,
-} from "./WorkPage.module.css";
+import { content, top, title, tabs, images, carousel } from "./WorkPage.module.css";
 import Carousel from "../component/Carousel/Carousel";
 import placeholder from "../images/me.png";
 import AppHeader from "../component/AppHeader/AppHeader";
 
-
 const ProjectPage = () => {
-  
+
   return (
     <div>
-      <AppHeader/>
+      <AppHeader />
       <div className={content}>
         <div className={top}>
           <div className={title}>WORKS</div>
           <div className={tabs}> hi</div>
         </div>
       </div>
-      
-        <Carousel>
+      <div
+        className={carousel}
+      >
+        <Carousel >
+         
           <img className={images} src={placeholder} alt="placeholder" />
           <img className={images} src={placeholder} alt="placeholder" />
           <img className={images} src={placeholder} alt="placeholder" />
@@ -31,7 +27,7 @@ const ProjectPage = () => {
           <img className={images} src={placeholder} alt="placeholder" />
         </Carousel>
       </div>
-      
+    </div>
   );
 };
 
