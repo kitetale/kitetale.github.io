26d7c81b7e62b7a4dce5856767bf1bd0820c82c4
Ashley Kim <kitetale248@gmail.com>
2022-12-20 02:02:12 -0500

    added intro message on landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React from "react";
 import AppHeader from "../component/AppHeader/AppHeader";
-import {fullsize} from "./LandingPage.module.css"
+import { overall, fullsize, msg } from "./LandingPage.module.css";
 
-import ThreeLazy from '../component/three-lazy';
-import Shapes from '../component/three-main';
+import ThreeLazy from "../component/three-lazy";
+import Shapes from "../component/three-main";
 //import Type from '../component/type-cloud';
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className={overall}>
       <AppHeader />
-        <div className={fullsize} >
-          {/* <Type/> */}
-          {/* <ThreeLazy/> */}
-          <Shapes/>
-        </div>
+      <div className={msg}>
+        <h1>Hello! I'm Ashley &#128526;</h1>
+        <h2>
+          I’m an interaction designer and a frontend developer who loves visual
+          effects, problem solving, and storytelling.
+        </h2>
+      </div>
+      <div className={fullsize}>
+        {/* <Type/> */}
+        {/* <ThreeLazy/> */}
+        <Shapes />
+      </div>
+      
     </div>
   );
 };
