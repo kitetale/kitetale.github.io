2839840be00618f2ae55267aaedcede0837c96c2
Ashley Kim <kitetale248@gmail.com>
2022-12-22 00:12:49 -0700

    added footer to work page

--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {
+  overall,
   content,
   top,
   title,
   tabs,
   images,
   carousel,
+  footer,
+  footDescrip
 } from "./WorkPage.module.css";
 import Carousel from "../../component/Carousel/Carousel";
 import placeholder from "../../images/me.png";
@@ -16,6 +19,7 @@ import { Link, graphql } from "gatsby";
 const ProjectPage = ({ data }) => {
   return (
     <div>
+    <div className={overall}>
       <AppHeader />
       <div className={content}>
         <div className={top}>
@@ -27,33 +31,27 @@ const ProjectPage = ({ data }) => {
         <Carousel show={4.3} infiniteLoop={true}>
           <img className={images} src={placeholder} alt="placeholder" />
           <img className={images} src={placeholder} alt="placeholder" />
+          <img className={images} src={placeholder} alt="placeholder" />
+          <img className={images} src={placeholder} alt="placeholder" />
+          <img className={images} src={placeholder} alt="placeholder" />
 
-          {data.allMdx.nodes.map((node) => (
+          {/* {data.allMdx.nodes.map((node) => (
             <Link to={`/works/${node.frontmatter.slug}`}>
               <img
                 className={images}
-                src={node.frontmatter.hero_img.absolutePath}
+                src={node.frontmatter.hero_img}
                 alt={node.frontmatter.hero_img_alt}
               />
             </Link>
-            // <article
-            //   key={node.id}
-            //   className={`filterArt ${node.frontmatter.tag}`}
-            // >
-            //   <div className={images}>
-            //     <Link to={`/works/${node.slug}`}>
-            //       <GatsbyImage
-            //         className={images}
-            //         image={getImage(node.frontmatter.hero_img.absolutePath)}
-            //         alt={node.frontmatter.hero_img_alt}
-            //       />
-            //     </Link>
-            //   </div>
-            // </article>
-          ))}
+          ))} */}
         </Carousel>
       </div>
     </div>
+    <footer className={footer}>(ง˙∇˙)ว © 2023 Ashley Kim (ว˙∇˙)ง</footer>
+    <footer className={footDescrip}>
+      Built with Gatsby {"&"} React! Last updated in January 2023.
+    </footer>
+  </div>
   );
 };
 
