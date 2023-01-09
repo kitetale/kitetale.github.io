42b0b0ba88473a4a9ec745df2c21015866f24600
Ashley Kim <kitetale248@gmail.com>
2023-01-08 20:58:34 -0500

    mobile menu animation implemented

--- src/component/AppHeader/AppHeader.js
+++ src/component/AppHeader/AppHeader.js
@@ -7,34 +7,56 @@ import {
   logo,
   words,
   logoUnderline,
+  menu,
+  hamburger,
 } from "./AppHeader.module.css";
 import { Link } from "gatsby";
 import AshleyKim from "../../images/AshleyKim.svg";
 
 const AppHeader = () => {
   return (
-    <div className={appHeader}>
-      <div className={words}>
-        <title>Ashley Kim</title>
-        <header className={header}>
-          <Link to="/" className={logoUnderline}>
-            <img src={AshleyKim} alt="Ashley Kim" className={logo} />
-          </Link>
-        </header>
-        <nav className={navLink}>
-          <ul className={navLink}>
-            <li className={navLinkItem}>
-              <Link to="/works">works</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/about">about</Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/resume">resume</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      <div className={appHeader}>
+        <div className={words}>
+          <title>Ashley Kim</title>
+          <header className={header}>
+            <Link to="/" className={logoUnderline}>
+              <img src={AshleyKim} alt="Ashley Kim" className={logo} />
+            </Link>
+          </header>
+          <nav className={navLink}>
+            <ul className={navLink}>
+              <li className={navLinkItem}>
+                <Link to="/works">WORKS</Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li className={navLinkItem}>
+                <Link to="/resume">RESUME</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
+
+      <label>
+        <input type="checkbox" />
+        <span class={menu}>
+          <span class={hamburger}></span>
+        </span>
+        <ul>
+          <li>
+            <Link to="/works">WORKS</Link>
+          </li>
+          <li>
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li>
+            <Link to="/resume">RESUME</Link>
+          </li>
+        </ul>
+      </label>
     </div>
   );
 };
